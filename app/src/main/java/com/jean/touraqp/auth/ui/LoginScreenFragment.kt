3491bd364bfea17b08002abdc7cff800be69a812
jeanspkdz <jeanchua1304@gmail.com>
2024-10-26 16:15:39 -0500

    feat(auth): Add navigation from login to register screen

--- app/src/main/java/com/jean/touraqp/auth/ui/LoginScreenFragment.kt
+++ app/src/main/java/com/jean/touraqp/auth/ui/LoginScreenFragment.kt
@@ -5,8 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.jean.touraqp.R
+import com.jean.touraqp.databinding.FragmentLoginScreenBinding
 
 class LoginScreenFragment : Fragment(R.layout.fragment_login_screen) {
 
+    private var loginScreenBinding : FragmentLoginScreenBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentLoginScreenBinding.bind(view)
+        loginScreenBinding = binding
+
+        binding.btnToRegister.setOnClickListener(){
+            findNavController().navigate(R.id.action_loginScreenFragment_to_registerScreenFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        loginScreenBinding = null
+    }
 }
--- app/src/main/java/com/jean/touraqp/auth/ui/RegisterScreenFragment.kt
+++ app/src/main/java/com/jean/touraqp/auth/ui/RegisterScreenFragment.kt
@@ -5,9 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.jean.touraqp.R
+import com.jean.touraqp.databinding.FragmentLoginScreenBinding
+import com.jean.touraqp.databinding.FragmentRegisterScreenBinding
 
 
 class RegisterScreenFragment : Fragment(R.layout.fragment_register_screen) {
+    private var registerScreenBinding : FragmentRegisterScreenBinding? = null
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentRegisterScreenBinding.bind(view)
+        registerScreenBinding = binding
+
+        binding.btnToRegister.setOnClickListener(){
+            findNavController().navigate(R.id.action_registerScreenFragment_to_loginScreenFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        registerScreenBinding = null
+    }
 }
